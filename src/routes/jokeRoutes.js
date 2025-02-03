35d226bf54ba159d61fc7f69e6f68fa7f6ba0a8c
Omer Sevik <omer.sevik@hotmail.fr>
2025-02-03 21:04:39 +0100

    init swagger

--- src/routes/jokeRoutes.js
+++ src/routes/jokeRoutes.js
@@ -1,20 +1,108 @@
 const express = require('express');
 const { getJokes, getJokeById, getRandomJoke, createJoke } = require('../controllers/jokeController');
-
 const router = express.Router();
 
 console.log("Définition des routes pour les blagues");
 
-// Route pour récupérer une blague aléatoire
+/**
+ * @swagger
+ * /api/blagues/random:
+ *   get:
+ *     summary: Récupère une blague aléatoire
+ *     responses:
+ *       200:
+ *         description: Une blague aléatoire
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 question:
+ *                   type: string
+ *                 answer:
+ *                   type: string
+ */
 router.get('/random', getRandomJoke);
 
-// Route pour récupérer toutes les blagues
+/**
+ * @swagger
+ * /api/blagues:
+ *   get:
+ *     summary: Récupère toutes les blagues
+ *     responses:
+ *       200:
+ *         description: Une liste de toutes les blagues
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   question:
+ *                     type: string
+ *                   answer:
+ *                     type: string
+ */
 router.get('/', getJokes);
 
-// Route pour récupérer une blague par ID
+/**
+ * @swagger
+ * /api/blagues/{id}:
+ *   get:
+ *     summary: Récupère une blague par son ID
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID de la blague
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: La blague correspondante
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 question:
+ *                   type: string
+ *                 answer:
+ *                   type: string
+ *       404:
+ *         description: Blague non trouvée
+ */
 router.get('/:id', getJokeById);
 
-// Route pour créer une blague
+/**
+ * @swagger
+ * /api/blagues:
+ *   post:
+ *     summary: Crée une nouvelle blague
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               question:
+ *                 type: string
+ *               answer:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: La blague a été créée
+ *       400:
+ *         description: Données invalides
+ */
 router.post('/', createJoke);
 
 module.exports = router;
